30c1cd44a21ac59e1990c256fe5a5b14c196f18f
archie.charisiadis <archie.charisiadis@accenture.com>
2017-07-17 18:54:52 +0100

    one

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello CONTINUOUS DELIVERY BABYYYYY!!!");
+  res.send("Hello CONTINUOUS DELIVERY BABYYY!!!");
 });
 
 app.listen(4000);
